4850f94f29fddcc555de7f1bdbd24392b2a4dd62
modestukasai <modestukasai@gmail.com>
2016-07-06 21:33:56 +0300

    Checkstyle configuration and fixes

--- service-discovery/src/main/java/commons/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/commons/ServiceDiscoveryApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceDiscoveryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
 }
