ab0c0c89c3f7313b588ff90ddd370e4be45fa397
modestukasai <modestukasai@gmail.com>
2016-07-24 12:42:33 +0300

    Application addition as main project

---
+++ project-a/api/src/main/java/com/projecta/api/IndexController.java
@@ -0,0 +1,7 @@
+package com.projecta.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+}
--- project-a/application/src/main/java/com/projecta/Application.java
+++ project-a/application/src/main/java/com/projecta/Application.java
@@ -1,4 +1,4 @@
-package com.projecta.api;
+package com.projecta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
