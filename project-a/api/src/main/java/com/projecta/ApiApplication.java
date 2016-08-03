26432b13e16a6b855d78be08233f839d6ebd117f
Modestas Kazinauskas <modestas.kazinauskas@4finance.com>
2016-08-03 14:35:02 +0300

    Updated project api

--- project-a/api/src/main/java/com/projecta/ApiApplication.java
+++ project-a/api/src/main/java/com/projecta/ApiApplication.java
@@ -9,13 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class ApiApplication {
 
     @Autowired
     private UserRepository userRepository;
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
     @Bean
     public CommandLineRunner init() {
