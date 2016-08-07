271825a52c562e031d49798993cd640e7e8c2833
Modestas Kazinauskas <modestukasai@gmail.com>
2016-08-07 23:36:05 +0300

    Eureka server and client enabled with config client

--- service-discovery/src/main/java/commons/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/commons/ServiceDiscoveryApplication.java
@@ -2,7 +2,10 @@ package commons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryApplication {
 
