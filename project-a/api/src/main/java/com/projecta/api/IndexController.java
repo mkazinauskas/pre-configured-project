bccf47c195978d8f2ead1f13a785853fea72b691
Modestas Kazinauskas <modestukasai@gmail.com>
2016-08-07 22:35:28 +0300

    Enabled git config server

--- project-a/api/src/main/java/com/projecta/api/IndexController.java
+++ project-a/api/src/main/java/com/projecta/api/IndexController.java
@@ -1,7 +1,19 @@
 package com.projecta.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class IndexController {
+    @Value(value = "${sample.property}")
+    private String sampleProperty;
+
+    @RequestMapping(value = "/sample-property", method = RequestMethod.GET)
+    public String getSampleProperty() {
+        return sampleProperty;
+    }
 }
