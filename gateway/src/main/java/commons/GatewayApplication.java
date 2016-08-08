5f28db77e3d221ced124fc60460d1c5073bcfb36
Modestas Kazinauskas <modestukasai@gmail.com>
2016-08-08 21:21:22 +0300

    Gateway update to support services

--- gateway/src/main/java/commons/GatewayApplication.java
+++ gateway/src/main/java/commons/GatewayApplication.java
@@ -2,6 +2,8 @@ package commons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
