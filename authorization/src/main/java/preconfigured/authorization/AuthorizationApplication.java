e30708542b3eec8c5047551f2ebddc1920c9a0e4
Modestas Kazinauskas <modestukasai@gmail.com>
2016-08-16 21:35:38 +0300

    Additional oauth users

--- authorization/src/main/java/preconfigured/authorization/AuthorizationApplication.java
+++ authorization/src/main/java/preconfigured/authorization/AuthorizationApplication.java
@@ -1,28 +1,30 @@
 package preconfigured.authorization;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 
-@RestController
 @EnableAuthorizationServer
 @EnableResourceServer
 @SpringBootApplication
 public class AuthorizationApplication {
 
-    @RequestMapping("/user")
-    public Principal user(Principal user) {
-        return user;
-    }
-
-
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationApplication.class, args);
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return username ->
+                new User(
+                        "user1", "password", true, true, true, true,
+                        AuthorityUtils.createAuthorityList("ADMIN")
+                );
     }
 }
