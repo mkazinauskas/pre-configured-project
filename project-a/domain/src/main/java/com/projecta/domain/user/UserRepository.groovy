9e7d4eee71ce7fe17d1c85068f0de86948c348f2
Modestas Kazinauskas <modestas.kazinauskas@4finance.com>
2016-08-18 14:32:46 +0300

    Project a domain update

--- project-a/domain/src/main/java/com/projecta/domain/user/UserRepository.groovy
+++
@@ -1,8 +0,0 @@
-package com.projecta.domain.user
-
-import groovy.transform.CompileStatic
-import org.springframework.data.jpa.repository.JpaRepository
-
-@CompileStatic
-interface UserRepository extends JpaRepository<User, Long> {
-}
