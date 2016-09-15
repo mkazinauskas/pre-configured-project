b6f1fc2b9794a774ed157901f14bac0c97b1c9ff
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2016-09-15 22:22:00 +0300

    Removal of unused code

--- project-a/api/src/main/java/projecta/ApiApplication.java
+++ project-a/api/src/main/java/projecta/ApiApplication.java
@@ -1,13 +1,8 @@
 package projecta;
 
-import preconfigured.authorization.projecta.domain.user.User;
-import preconfigured.authorization.projecta.domain.user.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableEurekaClient
@@ -15,14 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 @SpringBootApplication
 public class ApiApplication {
 
-    @Autowired
-    private UserRepository userRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
-    }
-    @Bean
-    public CommandLineRunner init() {
-        return args -> userRepository.save(new User("test@email.com"));
     }
 }
