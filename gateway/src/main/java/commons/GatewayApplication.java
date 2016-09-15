97842794968e551f2167e59928657c23218ba8db
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2016-09-15 21:59:31 +0300

    Gateway authorization

--- gateway/src/main/java/commons/GatewayApplication.java
+++ gateway/src/main/java/commons/GatewayApplication.java
@@ -2,9 +2,11 @@ package commons;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
+@EnableOAuth2Sso
 @SpringBootApplication
 public class GatewayApplication {
 
