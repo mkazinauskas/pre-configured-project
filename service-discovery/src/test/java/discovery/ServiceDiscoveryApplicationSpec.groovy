379071124abcbbe2353a346c7423f15df22de7b9
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2016-09-15 23:09:41 +0300

    Junit to spock test

---
+++ service-discovery/src/test/java/discovery/ServiceDiscoveryApplicationSpec.groovy
@@ -0,0 +1,20 @@
+package discovery
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Specification
+
+@ContextConfiguration
+@SpringBootTest
+class ServiceDiscoveryApplicationSpec extends Specification {
+
+    @Autowired
+    private ApplicationContext applicationContext
+
+    def 'should load context'() {
+        expect:
+            applicationContext != null
+    }
+}
