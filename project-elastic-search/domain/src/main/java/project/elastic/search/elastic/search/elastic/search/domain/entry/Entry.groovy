8f82e7100d2edb02b2181b79f33efdda74b49830
Modestas Kažinauskas <modestas.kazinauskas@4finance.com>
2017-01-16 16:45:25 +0200

    Codenarc fixes

--- project-elastic-search/domain/src/main/java/project/elastic/search/elastic/search/elastic/search/domain/entry/Entry.groovy
+++ project-elastic-search/domain/src/main/java/project/elastic/search/elastic/search/elastic/search/domain/entry/Entry.groovy
@@ -6,8 +6,6 @@ import org.springframework.data.elasticsearch.annotations.Document
 import org.springframework.data.elasticsearch.annotations.Field
 import org.springframework.data.elasticsearch.annotations.FieldIndex
 import org.springframework.data.elasticsearch.annotations.FieldType
-import org.springframework.data.elasticsearch.annotations.InnerField
-import org.springframework.data.elasticsearch.annotations.MultiField
 
 import static org.apache.commons.lang.RandomStringUtils.randomAlphanumeric
 
